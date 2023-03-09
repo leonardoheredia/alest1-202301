f2f199702952f2ecad4bde8dead4c1ee9cd7c2e3
leonardoheredia <leonardo@heredia.com.br>
2023-03-09 07:37:39 -0300

    Arquivos para aula 04.

--- aula04_complexidade_notacao/AnaliseBubbleSort.java
+++ aula04_complexidade_notacao/AnaliseBubbleSort.java
@@ -5,15 +5,24 @@ import arrays.ArrayUtils;
 public class AnaliseBubbleSort {
 
     public static void main(String[] args) {
-        int[] meuArray = {70, 60, 40, 30, 10};
 
+        System.out.println("N;bubble");
+        for (int N = 5; N < 100; N++) {
+            int[] meuArray = new int[N];
+            ArrayUtils.preencherArrayComValoresInteirosAleatorios(meuArray, 100, true);
+            BubbleSort bs = new BubbleSort();
+            bs.ordenar(meuArray);
+            int operacoes = bs.getOperacoes();
+            System.out.println(N + ";" + operacoes);
+        }
+        /*
         ArrayUtils.imprimir(meuArray);
         BubbleSort bs = new BubbleSort();
         bs.ordenar(meuArray);
         int operacoes = bs.getOperacoes();
         ArrayUtils.imprimir(meuArray);
         System.out.println("Ordenado apos " + operacoes + " operacoes ");
-
+        */
     }
 
 }
