cb3f2ed65056c5b00668b529b57b6e1e5f5ed3e3
leonardoheredia <leonardo@heredia.com.br>
2023-03-13 22:21:53 -0300

    Arquivos para aula 05. Respostas exercicios.

--- aula05_recursao/Exercicios.java
+++ aula05_recursao/Exercicios.java
@@ -10,11 +10,38 @@ public class Exercicios {
     5) Implementar um método recursivo que faça uma busca binária em um array de inteiros ordenado.
 */
     public static void main(String[] args) {
-        System.out.println(somatorio(200));
+        //System.out.println(somatorio(200));
+        escrever("que aula demais!!! nem quero saber da chamada!", 100);
     }
     public static int somatorio(int numero) {
         System.out.println(numero);
         if(numero==0) return 0;
         else return numero + somatorio(numero - 1);
     }
+
+    public static void escrever(String mensagem, int n) {
+        if(n == 0) return;
+        else {
+            System.out.println(mensagem);
+            escrever(mensagem, n - 1);
+        };
+    }
+
+    //Dica para o 3
+    //[ 12, 90, 33, 40]
+    //  0   1    2   4
+    //  n = 4  --> 4 troca com o 0
+    //  n = 3  --> 3 troca com o 1
+    //  n = 2  --> terminou
+
+
+    //[12,  90,  33,  40,  67,  89,  34]
+    // 0    1    2     3    4    5    6
+    // n = 6 --> 6 troca com o 0
+    // n = 5 --> 5 troca com o 1
+    // n = 4 --> 4 troca com o 2
+    // n = 3 --> terminou
+    //resultado
+    //[34,  89, 67,  40,  33,  90,  12]
+
 }
