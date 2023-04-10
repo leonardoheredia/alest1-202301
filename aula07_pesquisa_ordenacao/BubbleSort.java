20c3a5e1259fa6ff83dbc6e66ea1d990d465a33b
leonardoheredia <leonardo@heredia.com.br>
2023-04-10 17:06:22 -0300

    Aula 12 (Filas). Aula 13 (Pilhas)

--- aula07_pesquisa_ordenacao/BubbleSort.java
+++ aula07_pesquisa_ordenacao/BubbleSort.java
@@ -4,7 +4,19 @@ import arrays.ArrayUtils;
 
 public class BubbleSort {
 
-    private int operacoes; //propriedade para guardar o numero de operacoes realizadas
+    public long getOperacoes() {
+        return operacoes;
+    }
+
+    public long getTempoInicio() {
+        return tempoInicio;
+    }
+
+    public long getTempoFim() {
+        return tempoFim;
+    }
+
+    private long operacoes; //propriedade para guardar o numero de operacoes realizadas
     private long tempoInicio; //usado para guardar o inicio da execucao
     private long tempoFim;
 
@@ -15,15 +27,15 @@ public class BubbleSort {
         for (int i = 0; i < tamanho; i++) {
             for (int j = 0; j < tamanho-1-i; j++) {
                 operacoes++;
-                System.out.printf("Compara %d com %d%n", arrayInteiros[j+1], arrayInteiros[j]);
+                //System.out.printf("Compara %d com %d%n", arrayInteiros[j+1], arrayInteiros[j]);
                 if(arrayInteiros[j]>arrayInteiros[j+1]) {
-                    System.out.printf("Troca %d por %d%n", arrayInteiros[j+1], arrayInteiros[j]);
+                    //System.out.printf("Troca %d por %d%n", arrayInteiros[j+1], arrayInteiros[j]);
                     int temp = arrayInteiros[j];
                     arrayInteiros[j] = arrayInteiros[j+1];
                     arrayInteiros[j+1] = temp;
                     operacoes++;
                 }
-                ArrayUtils.imprimir(arrayInteiros);
+                //ArrayUtils.imprimir(arrayInteiros);
             }
         }
         tempoFim = System.nanoTime();
